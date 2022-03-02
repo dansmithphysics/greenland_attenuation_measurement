b8aefb79a2e99845e776c297e25fb917a7976861
Daniel Smith <danielsmith@uchicago.edu>
2022-03-01 19:39:33 -0600

    Cleaning up files to calculate the uncertainty on certain quantities

--- A03_calc_uncertainty_match.py
+++ A03_calc_uncertainty_match.py
@@ -2,86 +2,142 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 
-air_file_names = glob.glob("./data_raw/LPDA_Summit_Measurements/*AIR*LPDA*S11*csv")
-ice_file_names = glob.glob("./data_raw/LPDA_Summit_Measurements/*ICE*LPDA*S11*csv")
 
-ice_file_names = np.array([file_name for file_name in ice_file_names if not("ORIG" in file_name or "CROSS" in file_name)])
+def load_s11_files(file_names):
 
-plt.figure()
-for file_name in np.append(air_file_names, ice_file_names):
-    print(file_name)
-    freq, s11 = np.loadtxt(file_name, delimiter=",", comments = ['!', 'BEGIN', 'END'], unpack = True)
+    s11s = {}
+    freqs = {}
+    for file_name in np.append(air_file_names, ice_file_names):
 
-    if("AIR" in file_name):
-        color = 'red'
-    else:
-        color = 'blue'
+        freq, s11 = np.loadtxt(file_name,
+                               delimiter=",",
+                               comments=['!', 'BEGIN', 'END'],
+                               unpack=True)
+        s11s[file_name] = s11
+        freqs[file_name] = freq
 
-    plt.plot(freq * 1e-6, np.power(10.0, s11 / 10.0), color = color, alpha = 0.5)        
+    return freqs, s11s
 
-plt.plot([], [], color = 'red', alpha = 0.5, label = "In Air")    
-plt.plot([], [], color = 'blue', alpha = 0.5, label = "In Ice")
-    
-plt.xlim(0.0, 1000.0)
-plt.ylim(0.0, 0.5)
-plt.xlabel("Freq. [MHz]")
-plt.ylabel("S11 [dB]")
-plt.legend()
-plt.grid()
-plt.savefig("./plots/A03_calc_uncertainty_match.png", dpi = 300)
 
-plt.figure()
+if __name__ == "__main__":
 
-maxs = np.zeros(len(s11))
-mins = np.ones(len(s11)) * 1000.0
+    air_file_names = glob.glob("./data_raw/LPDA_Summit_Measurements/*AIR*LPDA*S11*csv")
+    ice_file_names = glob.glob("./data_raw/LPDA_Summit_Measurements/*ICE*LPDA*S11*csv")    
+    file_names = np.append(air_file_names, ice_file_names)
 
-for ice_file_name in ice_file_names:
-    for air_file_name in air_file_names:
+    freqs, s11s = load_s11_files(file_names)
 
-        freq, ice_s11 = np.loadtxt(ice_file_name, delimiter=",", comments = ['!', 'BEGIN', 'END'], unpack = True)
-        freq, air_s11 = np.loadtxt(air_file_name, delimiter=",", comments = ['!', 'BEGIN', 'END'], unpack = True)
+    plt.figure(figsize=(5, 4))
+    for file_name in file_names:
+        print(file_name)
+        s11 = s11s[file_name]
+        freq = freqs[file_name]
 
-        ratio = ((1.0 - np.power(10.0, ice_s11 / 20.0)) / (1.0 - np.power(10.0, air_s11 / 20.0)))
+        if("AIR" in file_name):
+            color = 'red'
+        else:
+            color = 'blue'
 
-        for i in range(len(ratio)):
-            if(ratio[i] > maxs[i]):
-                maxs[i] = ratio[i]
-            if(ratio[i] < mins[i]):
-                mins[i] = ratio[i]
-            
-plt.fill_between(freq * 1e-6, maxs, mins, color = 'black', alpha = 0.1)
+        plt.plot(freq * 1e-6, s11,
+                 color=color, alpha=0.4)
 
-mean = 1.05
-up = 0.05
-down = -0.05
+    plt.plot([], [], color='red', alpha=0.5, label="In Air")
+    plt.plot([], [], color='blue', alpha=0.5, label="In Ice")
 
-plt.axhline(mean + up, color = 'black', linestyle = "--", label = r"$T_{ratio} = 1.05\pm0.05$")
-plt.axhline(mean + down, color = 'black', linestyle = "--")
-plt.axvline(145.0, color = 'red', linestyle = '--', label = "Bandpass Filter")
-plt.axvline(575.0, color = 'red', linestyle = '--')
-plt.ylim(0.7, 1.3)
-plt.xlim(0.0, 750.0)
-plt.xlabel("Freq. [MHz]")
-plt.ylabel("T$_{ratio}$, (1.0 - S11$_{ice}$) / (1.0 - S11$_{air}$)")
-plt.legend()
-plt.grid()
-plt.savefig("./plots/A03_calc_unceratinty_match_t_ratio_with_errors.png", dpi = 300)
+    plt.xlim(0.0, 1000.0)
+    plt.ylim(-50.0, 0.0)
+    plt.xlabel("Frequency [MHz]")
+    plt.ylabel(r"$S_{11}$ [dB]")
+    plt.legend()
+    plt.grid()
+    plt.tight_layout()
 
-entries = (maxs + mins)[np.logical_and(freq > 150.0e6, freq < 550.0e6)] / 2.0
-entries = np.sort(entries)
-cumsum = np.cumsum(np.arange(len(entries)))
-cumsum = np.array(cumsum) / float(cumsum[-1])
+    plt.savefig("./plots/A03_calc_uncertainty_match_s11.png",
+                dpi=300)
 
-entries_min = entries[np.argmin(np.abs(cumsum - (0.5 - 0.341)))]
-entries_mid = entries[np.argmin(np.abs(cumsum - (0.5 - 0.000)))]
-entries_max = entries[np.argmin(np.abs(cumsum - (0.5 + 0.341)))]
+    plt.figure()
+    for file_name in np.append(air_file_names, ice_file_names):
+        print(file_name)
+        s11 = s11s[file_name]
+        freq = freqs[file_name]
 
-print("%f (+%f)(-%f)" % (entries_mid, entries_mid - entries_min, entries_max - entries_mid))
+        if("AIR" in file_name):
+            color = 'red'
+        else:
+            color = 'blue'
 
-plt.figure()
-plt.plot(entries, cumsum)
-plt.axvline(entries_min, color = 'red', linestyle = '--')
-plt.axvline(entries_mid, color = 'red', linestyle = '--', linewidth = 2.0, label = r"Depth: $3045^{+41}_{-45}$ m")
-plt.axvline(entries_max, color = 'red', linestyle = '--')
+        plt.plot(freq * 1e-6, np.power(10.0, s11 / 10.0),
+                 color=color, alpha=0.5)
 
-plt.show()
+    plt.plot([], [], color='red', alpha=0.5, label="In Air")
+    plt.plot([], [], color='blue', alpha=0.5, label="In Ice")
+
+    plt.xlim(0.0, 1000.0)
+    plt.ylim(0.0, 0.5)
+    plt.xlabel("Freq. [MHz]")
+    plt.ylabel("S11 [dB]")
+    plt.legend()
+    plt.grid()
+
+    plt.savefig("./plots/A03_calc_uncertainty_match.png",
+                dpi=300)
+
+    plt.figure()
+
+    maxs = np.zeros(len(s11))
+    mins = np.ones(len(s11)) * 1000.0
+
+    for ice_file_name in ice_file_names:
+        for air_file_name in air_file_names:
+
+            ice_s11 = s11s[ice_file_name]
+            air_s11 = s11s[air_file_name]
+            freq = freqs[file_name]
+
+            ratio = ((1.0 - np.power(10.0, ice_s11 / 20.0)) / (1.0 - np.power(10.0, air_s11 / 20.0)))
+
+            for i in range(len(ratio)):
+                if(ratio[i] > maxs[i]):
+                    maxs[i] = ratio[i]
+                if(ratio[i] < mins[i]):
+                    mins[i] = ratio[i]
+
+    plt.fill_between(freq * 1e-6, maxs, mins, color='black', alpha=0.1)
+
+    mean = 1.00
+    up = 0.05
+    down = -0.05
+
+    plt.axhline(mean + up, color='black', linestyle="--", label=r"$T_{ratio}=1.00\pm0.05$")
+    plt.axhline(mean + down, color='black', linestyle="--")
+    plt.axvline(145.0, color='red', linestyle='--', label="Bandpass Filter")
+    plt.axvline(575.0, color='red', linestyle='--')
+    plt.ylim(0.7, 1.3)
+    plt.xlim(0.0, 750.0)
+    plt.xlabel("Freq. [MHz]")
+    plt.ylabel("T$_{ratio}$, (1.0 - S11$_{ice}$) / (1.0 - S11$_{air}$)")
+    plt.legend()
+    plt.grid()
+
+    plt.savefig("./plots/A03_calc_unceratinty_match_t_ratio_with_errors.png",
+                dpi=300)
+
+    entries = (maxs + mins)[np.logical_and(freq > 150.0e6, freq < 550.0e6)] / 2.0
+    entries = np.sort(entries)
+    cumsum = np.cumsum(np.ones(len(entries)))
+    cumsum = np.array(cumsum) / float(cumsum[-1])
+
+    entries_min = entries[np.argmin(np.abs(cumsum - (0.5 - 0.341)))]
+    entries_mid = entries[np.argmin(np.abs(cumsum - (0.5 - 0.000)))]
+    entries_max = entries[np.argmin(np.abs(cumsum - (0.5 + 0.341)))]
+
+    print("%f (+%f)(-%f)" % (entries_mid, entries_mid - entries_min, entries_max - entries_mid))
+
+    plt.figure()
+    plt.plot(entries, cumsum)
+    plt.axvline(entries_min, color='red', linestyle='--')
+    plt.axvline(entries_mid, color='red', linestyle='--',
+                linewidth=2.0, label=r"Depth: $3045^{+41}_{-45}$ m")
+    plt.axvline(entries_max, color='red', linestyle='--')
+
+    plt.show()
--- A03_calc_uncertainty_t0.py
+++ A03_calc_uncertainty_t0.py
@@ -3,74 +3,87 @@ import scipy.signal
 import numpy as np
 import matplotlib.pyplot as plt
 
-noise_times = [25.0e-6, 32.0e-6]
-window_length = 250
 
-data = np.load("./data_processed/averaged_in_ice_trace.npz")
-data_t = data["template_time"]
-data_trace = data["template_trace"]
+def integrated_window(data_trace, data_t):
+    data_power_mW = np.power(data_trace * 1e3, 2.0) / 50.0
+    time_length = window_length * (data_t[1] - data_t[0]) * 1e9
+    window = scipy.signal.windows.tukey(window_length, alpha=0.25)
 
-#####################
-#      Raw Data     #
-#####################
+    rolling = np.convolve(data_power_mW / time_length,
+                          window,
+                          'valid')
 
-noise_std = np.std(data_trace[np.logical_and(data_t > noise_times[0], data_t < noise_times[1])])
+    rolling_t = (data_t[(window_length - 1):] + data_t[:-(window_length - 1)]) / 2.0
 
-plt.figure()
-plt.title("Ice Echo Data from Bed Rock")
+    return rolling_t, rolling
 
-plt.plot(data_t * 1e6, 
-         data_trace * 1e3,
-         alpha = 1.0, color = 'black', linewidth = 1.0, label = "Data")
 
-plt.axhline(3.0 * noise_std * 1e3, color = 'red', linestyle = '--', label = "3x Noise RMS")
-plt.axvline(34.59, color = 'purple', linestyle = '-', label = "Start of G.B.")
-plt.axvline(35.05, color = 'purple', linestyle = "-.", label = "End of G.B.")
-plt.xlabel("Time Since Trigger $t_0$ [$\mu$s]")
-plt.ylabel("Trace [mV]")
-plt.xlim(33.0, 38.0)
-plt.ylim(-0.7, 0.7)
-plt.legend(loc = 'upper right')
-plt.grid()
+if __name__ == "__main__":
 
-plt.savefig("./plots/A03_calc_uncertainty_t0.png", dpi = 300)
+    noise_times = [25.0e-6, 32.0e-6]
+    window_length = 250
 
-#####################
-# Integrated Window #
-#####################
+    data = np.load("./data_processed/averaged_in_ice_trace.npz")
+    data_t = data["template_time"]
+    data_trace = data["template_trace"]
 
-data_power_mW = np.power(data_trace * 1e3, 2.0) / 50.0
-time_length = window_length * (data_t[1] - data_t[0]) * 1e9
-window = scipy.signal.windows.tukey(window_length, alpha = 0.25)
+    #####################
+    #      Raw Data     #
+    #####################
 
-rolling = np.convolve(data_power_mW / time_length,
-                      window,
-                      'valid')
+    # Calculate the standard deviation of noise.
+    noise_std = np.std(data_trace[np.logical_and(data_t > noise_times[0],
+                                                 data_t < noise_times[1])])
 
-rolling_t = (data_t[(window_length - 1):] + data_t[:-(window_length - 1)]) / 2.0
-    
-noise = rolling[np.logical_and(rolling_t > noise_times[0], rolling_t < noise_times[1])]
+    plt.figure()
+    plt.title("Ice Echo Data from Bed Rock")
 
-noise = np.sort(noise)
-cumsum = np.cumsum(np.arange(len(noise)))
-cumsum = np.array(cumsum) / float(cumsum[-1])
-noise_threshold = noise[np.argmin(np.abs(cumsum - 0.95))]
+    plt.plot(data_t * 1e6,
+             data_trace * 1e3,
+             alpha=1.0, color='black', linewidth=1.0, label="Data")
 
-plt.figure()
-plt.title("Ice Echo Data from Bed Rock, Integrated in Sliding %i ns Window" % int(250.0 * 1e9 * (data_t[1] - data_t[0])))
-plt.semilogy((data_t[:len(rolling)] + data_t[-len(rolling):]) / 2.0* 1e6,
-             rolling, alpha = 1.0, color = 'black', linewidth = 1.0, label = "Integrated Data")
+    plt.axhline(3.0 * noise_std * 1e3, color='red', linestyle='--', label="3x Noise RMS")
+    plt.axvline(34.59, color='purple', linestyle='-', label="Start of G.B.")
+    plt.axvline(35.05, color='purple', linestyle="-.", label="End of G.B.")
+    plt.xlabel("Time Since Trigger $t_0$ [$\mu$s]")
+    plt.ylabel("Trace [mV]")
+    plt.xlim(33.0, 38.0)
+    plt.ylim(-0.7, 0.7)
+    plt.legend(loc='upper right')
+    plt.grid()
 
-plt.axhline(noise_threshold, color = 'red', linestyle = '--', label = "95% CI of Noise")
-plt.axvline(34.59, color = 'purple', linestyle = '-', label = "Start of G.B.")
-plt.axvline(35.05, color = 'purple', linestyle = "-.", label = "End of G.B.")
-plt.xlabel("Time Since Trigger $t_0$ [$\mu$s]")
-plt.ylabel("Integrated Power [mW / ns]")
-plt.xlim(33.0, 38.0)
-plt.ylim(4e-6, 4e-2)
-plt.legend(loc = 'upper right')
-plt.grid()
+    plt.savefig("./plots/A03_calc_uncertainty_t0.png",
+                dpi=300)
 
-plt.savefig("./plots/A03_calc_uncertainty_t0_integrated.png", dpi = 300)
+    #####################
+    # Integrated Window #
+    #####################
 
-plt.show()
+    rolling_t, rolling = integrated_window(data_trace, data_t)
+
+    # Calculate a 95% CI of noise.
+    noise = rolling[np.logical_and(rolling_t > noise_times[0], rolling_t < noise_times[1])]
+    noise = np.sort(noise)
+    cumsum = np.cumsum(np.ones(len(noise)))
+    cumsum = np.array(cumsum) / float(cumsum[-1])
+    noise_threshold = noise[np.argmin(np.abs(cumsum - 0.95))]
+
+    plt.figure()
+    plt.title("Ice Echo Data from Bed Rock, Integrated in Sliding %i ns Window" %
+              int(250.0 * 1e9 * (data_t[1] - data_t[0])))
+    plt.semilogy((data_t[:len(rolling)] + data_t[-len(rolling):]) / 2.0 * 1e6,
+                 rolling, alpha=1.0, color='black', linewidth=1.0, label="Integrated Data")
+
+    plt.axhline(noise_threshold, color='red', linestyle='--', label="95% CI of Noise")
+    plt.axvline(34.59, color='purple', linestyle='-', label="Start of G.B.")
+    plt.axvline(35.05, color='purple', linestyle="-.", label="End of G.B.")
+    plt.xlabel("Time Since Trigger $t_0$ [$\mu$s]")
+    plt.ylabel("Integrated Power [mW / ns]")
+    plt.xlim(33.0, 38.0)
+    plt.ylim(4e-6, 4e-2)
+    plt.legend(loc='upper right')
+    plt.grid()
+
+    plt.savefig("./plots/A03_calc_uncertainty_t0_integrated.png", dpi=300)
+
+    plt.show()
