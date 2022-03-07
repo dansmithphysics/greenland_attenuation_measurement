Fixed a small bug in the units of the sliding integration window function

A03_calc_uncertainty_depth.py
@@ -83,7 +83,7 @@ if __name__ == "__main__":
     plt.xlabel(r"Absolute Time Since Pulser [$\mu$s]")
     plt.ylabel("Integrated Power [mW ns]")
     plt.xlim(33.0, 38.0)
-    plt.ylim(1e-9, 1e-6)
+    plt.ylim(1e-7, 1e-4)
 
     x_ticks = np.arange(33, 39, 1)
     x_ticks = np.append(x_ticks,

analysis_funcs.py
@@ -51,12 +51,12 @@ def power_integration(time, trace, window_length=250):
     """
     
     data_power_mW = np.power(trace, 2.0) / 50.0 * 1e3
-
-    time_length = window_length * (time[1] - time[0]) * 1e9
+    delta_t = (time[1] - time[0]) * 1e9
+    time_length = window_length * delta_t
 
     window = scipy.signal.windows.tukey(window_length, alpha=0.25)
 
-    rolling = np.convolve(data_power_mW / time_length,
+    rolling = np.convolve(data_power_mW * delta_t,
                           window,
                           'valid')
 